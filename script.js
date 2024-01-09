ee27126d6dc1666a0136a88c03789f25c3310e90
Murtaza Sadriwala <151193465+murtaza-sadri-19@users.noreply.github.com>
2024-01-09 11:56:09 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -2,3 +2,14 @@ function toggleContent(contentId) {
     var content = document.getElementById(contentId);
     content.classList.toggle("active");
 }
+function toggleContent(contentId) {
+    var content = document.getElementById(contentId);
+    content.style.display = (content.style.display === 'none' || content.style.display === '') ? 'block' : 'none';
+}
+
+function hideContentOnMouseOut(contentId) {
+    var content = document.getElementById(contentId);
+    content.style.display = 'none';
+}
+//  department javascript --------------------------------------------------------------------------------------------------------------------
+
